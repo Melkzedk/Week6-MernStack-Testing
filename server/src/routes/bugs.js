199dfef7afd7edac2db7e2ae43b67cb7208c746f
Melkzedk <melkywafula29@gmail.com>
2025-11-04 10:46:36 +0300

    changes

---
+++ server/src/routes/bugs.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/bugsController');
+
+router.post('/', controller.createBug);
+router.get('/', controller.getAllBugs);
+router.patch('/:id', controller.updateBug);
+router.delete('/:id', controller.deleteBug);
+
+module.exports = router;
