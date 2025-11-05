775cdb30869944986afa5d8b9527a85f85a1c7d7
Melkzedk <melkywafula29@gmail.com>
2025-11-05 16:43:00 +0300

    changes

--- server/src/index.js
+++ server/src/index.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.post('/api/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
 
 app.use('/api/bugs', bugsRouter);
 
