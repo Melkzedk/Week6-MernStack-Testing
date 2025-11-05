ec1bc1359c8650e4a7be6fbf9f9694122604c028
Melkzedk <melkywafula29@gmail.com>
2025-11-05 16:45:16 +0300

    small changes

--- server/src/models/Bug.js
+++ server/src/models/Bug.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const BugSchema = new mongoose.Schema({
 title: { type: String, required: true },
 description: { type: String },
-test: {type: String},
 status: { type: String, enum: ['open', 'in-progress', 'resolved'], default: 'open' },
 createdAt: { type: Date, default: Date.now }
 });
